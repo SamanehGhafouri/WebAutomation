c3de2b162384ffda19df21f464838ffbc759fa4a
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-02-15 13:45:45 -0500

    find the track by its className and test the click button to play it

--- bandleader.py
+++ bandleader.py
@@ -7,17 +7,28 @@ from selenium.webdriver.chrome.options import Options
 # assert opts.headless  # Operating in headless mode
 # browser = Chrome(options=opts)
 # browser.get('https://duckduckgo.com')
+#######################################################################
 
-# with GUI
+# # with GUI
+# browser = Chrome()
+# browser.get('https://duckduckgo.com')
+#
+# # query the input by passing 'real python' in input to search
+# search_form = browser.find_element_by_id('search_form_input_homepage')
+# search_form.send_keys('real python')
+# search_form.submit()
+#
+# # with this we can get the first item description in text in terminal
+# results = browser.find_elements_by_class_name('result')
+# print(results[0].text)
+
+#######################################################################
+# test the click button to play the music
 browser = Chrome()
-browser.get('https://duckduckgo.com')
+browser.get('https://bandcamp.com')
+# browser.find_element_by_class_name('playbutton').click()
 
-# query the input by passing 'real python' in input to search
-search_form = browser.find_element_by_id('search_form_input_homepage')
-search_form.send_keys('real python')
-search_form.submit()
-
-# with this we can get the first item description in text in terminal
-results = browser.find_elements_by_class_name('result')
-print(results[0].text)
-
+# find discover-item class
+tracks = browser.find_elements_by_class_name('discover-item')
+len(tracks)  # len of tracks
+tracks[3].click() # find the 4th track and click
