14a5ff7170698b1be706d7c322107582a8668a9d
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-02-18 16:40:48 -0500

    test file and created myhistory.csv by running the test to keep the record as a database of what music we listend

--- test.py
+++ test.py
@@ -7,16 +7,72 @@ from os.path import isfile
 import csv
 
 BANDCAMP_FRONTPAGE = 'https://bandcamp.com/'
+TrackRec = namedtuple('TrackRec', [
+                                    'title',
+                                    'artist',
+                                    'artist_url',
+                                    'album',
+                                    'album_url',
+                                    'timestamp'     # When you played it
+                                    ])
 
 
 class Test():
-    def __init__(self):
+    def __init__(self, csvpath = None):
+
+        self.database_path = csvpath
+        self.database = []
+
+        if isfile(self.database_path):
+            with open(self.database_path, newline='') as dbfile:
+                dbreader = csv.reader(dbfile)
+                next(dbreader)
+                self.database = [TrackRec._make(rec) for rec in dbreader]
+
         self.browser = Chrome()
         self.browser.get(BANDCAMP_FRONTPAGE)
 
         self._current_track_number = 1
         self.track_list = []
         self.tracks()
+
+        self.database = []
+        self._current_track_record = None
+
+        self.thread = Thread(target=self._maintain)
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.tracks()
+
+    def save_db(self):
+
+        with open(self.database_path, 'w', newline='') as dbfile:
+            dbwriter = csv.writer(dbfile)
+            dbwriter.writerow(list(TrackRec._fields))
+            for entry in self.database:
+                dbwriter.writerow(list(entry))
+
+    def _maintain(self):
+
+        while True:
+            self._update_db()
+            sleep(20)
+
+    def _update_db(self):
+
+        try:
+            check = (self._current_track_record is not None
+                     and self._current_track_record is not None
+                     and (len(self.database) == 0
+                          or self.database[-1] != self._current_track_record)
+                     and self.is_playing())
+            if check:
+                self.database.append(self._current_track_record)
+                self.save_db()
+
+        except Exception as e:
+            print('error while updating the db: {}'.format(e))
 
     def tracks(self):
 
@@ -62,6 +118,10 @@ class Test():
             self._current_track_number = track
             self.track_list[self._current_track_number - 1].click()
 
+        sleep(0.5)
+        if self.is_playing():
+            self._current_track_record = self.currently_playing()
+
     def play_next(self):
 
         if self._current_track_number < len(self.track_list):
@@ -74,15 +134,46 @@ class Test():
 
         self.play()
 
+    def is_playing(self):
+
+        playbtn = self.browser.find_element_by_class_name('playbutton')
+        return playbtn.get_attribute('class').find('playing') > -1
+
+    def currently_playing(self):
+
+        try:
+            if self.is_playing():
+                title = self.browser.find_element_by_class_name('title').text
+                album_detail = self.browser.find_element_by_css_selector('.detail-artist > a')
+                album_title = album_detail.text
+                album_url = album_detail.get_attribute('href').split('?')[0]
+                artist_detail = self.browser.find_element_by_css_selector('.detail-artist > a')
+                artist = artist_detail.text
+                artist_url = artist_detail.get_attribute('href').split('?')[0]
+                return TrackRec(title, artist, artist_url, album_title, album_url, ctime())
+        except Exception as e:
+            print('there was an error: {}'.format(e))
+
+        return None
+
+
 
 if __name__ == '__main__':
-    my_test = Test()
+    my_test = Test('myhistory.csv')
     my_test.tracks()
     my_test.catalogue_pages()
     my_test.more_tracks()
-    my_test.play(9)
+    my_test.play(1)
     my_test.play_next()
-    my_test.pause()
+    # my_test.pause()
+    playing = my_test.is_playing()
+    my_test.currently_playing()
+    my_test.save_db()
+    my_test._maintain()
+    my_test._update_db()
+
+
+
 
 
 
